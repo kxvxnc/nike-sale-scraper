d4c6774e22f3b8212241a48450b28c4df91264d8
Kevin Chen <potatokevin123@gmail.com>
2020-06-30 15:12:07 -0400

    API Fix and Removed Proxy Support

--- main.py
+++ main.py
@@ -9,32 +9,37 @@ profit_threshold = 40
 sale_threshold = 10
 
 def get_nike():
-    base_endpoint = "https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=7CE0EA46874C9E9F8DD84A713A17987F&endpoint="
-    query = "%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(US)%26filter%3Dlanguage(en)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(16633190-45e5-4830-a068-232ac7aea82c%2C5b21a62a-0503-400c-8336-3ccfbff2a684)%26anchor%3D60%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D60"
-    r = requests.get(f"{base_endpoint}{query}").json()
+    base_endpoint = "https://api.nike.com/cic/browse/v1"
+    query = {
+        "queryid": "products",
+        "anonymousId": "7CE0EA46874C9E9F8DD84A713A17987F",
+        "endpoint": "/product_feed/rollup_threads/v2?filter=marketplace%28US%29&filter=language%28en%29&filter=employeePrice%28true%29&filter=attributeIds%2816633190-45e5-4830-a068-232ac7aea82c%2C5b21a62a-0503-400c-8336-3ccfbff2a684%29&anchor=0&consumerChannelId=d9a5bc42-4b9c-4976-858a-f159cf99c647&count=50"
+    }
+    r = requests.get(base_endpoint, params = query).json()
 
     product_dict = {}
 
     while "next" in r["data"]["products"]["pages"]:
-        for object in r["data"]["products"]["objects"]:
-            main_sku = object["productInfo"][0]["merchProduct"]["styleColor"]
-            main_price = object["productInfo"][0]["merchPrice"]["currentPrice"]
-            product_dict[main_sku] = main_price * 0.8
+        print("Scraping page...")
+        for product in r["data"]["products"]["products"]:
+            main_sku = product["url"].split("/")[-1]
+            main_price = product["price"]["currentPrice"]
+            product_dict[main_sku] = main_price
+            for variant in product["colorways"]:
+                sku = variant["pdpUrl"].split("/")[-1]
+                price = variant["price"]["currentPrice"]
+                product_dict[sku] = price
 
-            for variant in object["rollup"]["threads"]:
-                variant_sku = variant["productInfo"][0]["merchProduct"]["styleColor"]
-                variant_price = variant["productInfo"][0]["merchPrice"]["currentPrice"]
-                product_dict[variant_sku] = variant_price * 0.8
+        query["endpoint"] = r['data']['products']['pages']['next']
 
-        next_page = urllib.parse.quote(r['data']['products']['pages']['next'], safe='')
-
-        r = requests.get(f"{base_endpoint}{next_page}").json()
+        r = requests.get(base_endpoint, params = query).json()
         if r["data"]["products"]["pages"]["next"] == "":
             break
-
+    
+    print(product_dict)
     return product_dict
 
-def get_stockx(sku, price, proxy):
+def get_stockx(sku, price):
     json_string = json.dumps({"params": f"query={sku}&hitsPerPage=20&facets=*"})
     byte_payload = bytes(json_string, 'utf-8')
     algolia = {
@@ -50,11 +55,8 @@ def get_stockx(sku, price, proxy):
         'appversion': '0.1',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
     }
-    proxy = {
-        "https": f"https://{proxy}",
-        "http": f"http://{proxy}"
-    }
-    r = requests.post("https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query", params=algolia, verify=False, data=byte_payload, timeout=30, proxies=proxy)
+
+    r = requests.post("https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query", params=algolia, verify=False, data=byte_payload, timeout=30)
     try:
         results = r.json()["hits"][0]
         apiurl = f"https://stockx.com/api/products/{results['url']}?includes=market,360&currency=USD"
@@ -68,7 +70,8 @@ def get_stockx(sku, price, proxy):
                 for size in sizes:
                     profit = round(((sizes[size]['market']['lowestAsk'] * 0.89) - price), 2)
                     if profit > profit_threshold:
-                        size_list += f"{sizes[size]['shoeSize']} "
+                        print(sizes[size]['shoeSize'])
+                        size_list += f"{sizes[size]['shoeSize']}"
 
                 with open("stockX.txt", "a") as f:
                     f.write(f"https://nike.com/t/-/{sku} {size_list} https://stockx.com/{results['url']}\n")
@@ -76,7 +79,7 @@ def get_stockx(sku, price, proxy):
         except:
             print(f"{response} {sku} is not profitable.")
     except:
-        print(f"{sku} not found on StockX.")
+        print(f"{r} {sku} not found on StockX.")
 
 # def get_goat():
 
@@ -93,10 +96,8 @@ def get_proxies(file):
 
 def main():
     products = get_nike()
-    proxy_list = get_proxies("proxies.txt")
     for sku, price in products.items():
-        proxy = random.choice(proxy_list)
-        get_stockx(sku, price, proxy)
+        get_stockx(sku, price)
         time.sleep(1)
 
 if __name__ == "__main__":
